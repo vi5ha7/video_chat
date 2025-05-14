0d15d7e57de74a96231f4f21d99328043f5c7233
vi5ha7 <vishalkno@gmail.com>
2025-05-14 07:56:12 +0530

    server.js

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const { v4: uuidv4 } = require("uuid");
+
 app.set("view engine", "ejs");
+
 const io = require("socket.io")(server, {
   cors: {
     origin: '*'
   }
 });
+
 const { ExpressPeerServer } = require("peer");
-const opinions = {
-  debug: true,
-}
+const opinions = { debug: true };
+
+const rooms = {};
 
 app.use("/peerjs", ExpressPeerServer(server, opinions));
 app.use(express.static("public"));
@@ -27,11 +30,26 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
-    setTimeout(()=>{
-      socket.to(roomId).broadcast.emit("user-connected", userId);
-    }, 1000)
+    
+    if (!rooms[roomId]) {
+      rooms[roomId] = [];
+    }
+
+    rooms[roomId].push(userId);
+
+    // Send existing users to new user
+    socket.emit("all-users", rooms[roomId].filter(id => id !== userId));
+
+    // Notify others about new user
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+
     socket.on("message", (message) => {
       io.to(roomId).emit("createMessage", message, userName);
+    });
+
+    socket.on("disconnect", () => {
+      rooms[roomId] = rooms[roomId].filter(id => id !== userId);
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
     });
   });
 });
