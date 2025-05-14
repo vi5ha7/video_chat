071667d5daa72ab7c43099e5e78a1c42a91235b1
vi5ha7 <vishalkno@gmail.com>
2025-05-14 08:02:26 +0530

    server.js

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
+const http = require("http");
+const server = http.createServer(app);
 const { v4: uuidv4 } = require("uuid");
-
-app.set("view engine", "ejs");
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: '*'
-  }
+    origin: "*",
+    methods: ["GET", "POST"],
+    transports: ["websocket"],
+  },
+  allowEIO3: true,
 });
 
 const { ExpressPeerServer } = require("peer");
-const opinions = { debug: true };
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+  path: '/',
+});
+
+app.use("/peerjs", peerServer);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 const rooms = {};
-
-app.use("/peerjs", ExpressPeerServer(server, opinions));
-app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.redirect(`/${uuidv4()}`);
@@ -30,11 +36,8 @@ app.get("/:room", (req, res) => {
 io.on("connection", (socket) => {
   socket.on("join-room", (roomId, userId, userName) => {
     socket.join(roomId);
-    
-    if (!rooms[roomId]) {
-      rooms[roomId] = [];
-    }
 
+    if (!rooms[roomId]) rooms[roomId] = [];
     rooms[roomId].push(userId);
 
     // Send existing users to new user
@@ -54,4 +57,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(process.env.PORT || 3030);
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
