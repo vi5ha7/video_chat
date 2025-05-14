c6ffc2335137683bc590084db4755fb8b09a0eae
vi5ha7 <vishalkno@gmail.com>
2025-05-14 08:20:51 +0530

    script.js

--- public/script.js
+++ public/script.js
@@ -1,124 +1,128 @@
-const socket = io("/");
-const videoGrid = document.getElementById("video-grid");
-const myVideo = document.createElement("video");
-myVideo.muted = true;
+document.addEventListener("DOMContentLoaded", () => {
+  const socket = io("/");
+  const videoGrid = document.getElementById("video-grid");
+  const myVideo = document.createElement("video");
+  myVideo.muted = true;
 
-const showChat = document.querySelector("#showChat");
-const backBtn = document.querySelector(".header__back");
-backBtn.addEventListener("click", () => {
-  document.querySelector(".main__left").style.display = "flex";
-  document.querySelector(".main__left").style.flex = "1";
-  document.querySelector(".main__right").style.display = "none";
-  document.querySelector(".header__back").style.display = "none";
-});
-showChat.addEventListener("click", () => {
-  document.querySelector(".main__right").style.display = "flex";
-  document.querySelector(".main__right").style.flex = "1";
-  document.querySelector(".main__left").style.display = "none";
-  document.querySelector(".header__back").style.display = "block";
-});
+  const showChat = document.querySelector("#showChat");
+  const backBtn = document.querySelector(".header__back");
 
-const user = prompt("Enter your name");
+  backBtn.addEventListener("click", () => {
+    document.querySelector(".main__left").style.display = "flex";
+    document.querySelector(".main__left").style.flex = "1";
+    document.querySelector(".main__right").style.display = "none";
+    document.querySelector(".header__back").style.display = "none";
+  });
 
-var peer = new Peer(undefined, {
-  host: window.location.hostname,
-  port: window.location.port || (location.protocol === 'https:' ? 443 : 80),
-  path: '/peerjs',
-  secure: location.protocol === 'https:',
-  debug: 3
-});
+  showChat.addEventListener("click", () => {
+    document.querySelector(".main__right").style.display = "flex";
+    document.querySelector(".main__right").style.flex = "1";
+    document.querySelector(".main__left").style.display = "none";
+    document.querySelector(".header__back").style.display = "block";
+  });
 
-let myVideoStream;
-navigator.mediaDevices.getUserMedia({
-  audio: true,
-  video: true,
-}).then((stream) => {
-  myVideoStream = stream;
-  addVideoStream(myVideo, stream);
+  const user = prompt("Enter your name") || "Anonymous";
 
-  peer.on("call", (call) => {
-    call.answer(stream);
+  const peer = new Peer(undefined, {
+    host: window.location.hostname,
+    port: window.location.port || (location.protocol === 'https:' ? 443 : 80),
+    path: '/peerjs',
+    secure: location.protocol === 'https:',
+    debug: 3
+  });
+
+  let myVideoStream;
+  navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true,
+  }).then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+
+    socket.on("all-users", (users) => {
+      users.forEach((userId) => {
+        connectToNewUser(userId, stream);
+      });
+    });
+  });
+
+  peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id, user);
+  });
+
+  const connectToNewUser = (userId, stream) => {
+    const call = peer.call(userId, stream);
     const video = document.createElement("video");
     call.on("stream", (userVideoStream) => {
       addVideoStream(video, userVideoStream);
     });
-  });
+  };
 
-  socket.on("user-connected", (userId) => {
-    connectToNewUser(userId, stream);
-  });
-
-  socket.on("all-users", (users) => {
-    users.forEach((userId) => {
-      connectToNewUser(userId, stream);
+  const addVideoStream = (video, stream) => {
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+      video.play();
+      videoGrid.append(video);
     });
+  };
+
+  let text = document.querySelector("#chat_message");
+  let send = document.getElementById("send");
+  let messages = document.querySelector(".messages");
+
+  send.addEventListener("click", () => {
+    if (text.value.length !== 0) {
+      socket.emit("message", text.value);
+      text.value = "";
+    }
   });
-});
 
-peer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, id, user);
-});
-
-function connectToNewUser(userId, stream) {
-  const call = peer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
+  text.addEventListener("keydown", (e) => {
+    if (e.key === "Enter" && text.value.length !== 0) {
+      socket.emit("message", text.value);
+      text.value = "";
+    }
   });
-}
 
-function addVideoStream(video, stream) {
-  video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
-    video.play();
-    videoGrid.append(video);
+  const inviteButton = document.querySelector("#inviteButton");
+  const muteButton = document.querySelector("#muteButton");
+  const stopVideo = document.querySelector("#stopVideo");
+
+  muteButton.addEventListener("click", () => {
+    const enabled = myVideoStream.getAudioTracks()[0].enabled;
+    myVideoStream.getAudioTracks()[0].enabled = !enabled;
+    muteButton.innerHTML = `<i class="fas ${enabled ? 'fa-microphone-slash' : 'fa-microphone'}"></i>`;
+    muteButton.classList.toggle("background__red", enabled);
   });
-}
 
-let text = document.querySelector("#chat_message");
-let send = document.getElementById("send");
-let messages = document.querySelector(".messages");
+  stopVideo.addEventListener("click", () => {
+    const enabled = myVideoStream.getVideoTracks()[0].enabled;
+    myVideoStream.getVideoTracks()[0].enabled = !enabled;
+    stopVideo.innerHTML = `<i class="fas ${enabled ? 'fa-video-slash' : 'fa-video'}"></i>`;
+    stopVideo.classList.toggle("background__red", enabled);
+  });
 
-send.addEventListener("click", () => {
-  if (text.value.length !== 0) {
-    socket.emit("message", text.value);
-    text.value = "";
-  }
-});
+  inviteButton.addEventListener("click", () => {
+    prompt("Copy this link and send it to people you want to meet with", window.location.href);
+  });
 
-text.addEventListener("keydown", (e) => {
-  if (e.key === "Enter" && text.value.length !== 0) {
-    socket.emit("message", text.value);
-    text.value = "";
-  }
-});
-
-const inviteButton = document.querySelector("#inviteButton");
-const muteButton = document.querySelector("#muteButton");
-const stopVideo = document.querySelector("#stopVideo");
-
-muteButton.addEventListener("click", () => {
-  const enabled = myVideoStream.getAudioTracks()[0].enabled;
-  myVideoStream.getAudioTracks()[0].enabled = !enabled;
-  muteButton.innerHTML = `<i class="fas ${enabled ? 'fa-microphone-slash' : 'fa-microphone'}"></i>`;
-  muteButton.classList.toggle("background__red", enabled);
-});
-
-stopVideo.addEventListener("click", () => {
-  const enabled = myVideoStream.getVideoTracks()[0].enabled;
-  myVideoStream.getVideoTracks()[0].enabled = !enabled;
-  stopVideo.innerHTML = `<i class="fas ${enabled ? 'fa-video-slash' : 'fa-video'}"></i>`;
-  stopVideo.classList.toggle("background__red", enabled);
-});
-
-inviteButton.addEventListener("click", () => {
-  prompt("Copy this link and send it to people you want to meet with", window.location.href);
-});
-
-socket.on("createMessage", (message, userName) => {
-  messages.innerHTML += `
-    <div class="message">
-      <b><i class="far fa-user-circle"></i> <span> ${userName === user ? "me" : userName}</span> </b>
-      <span>${message}</span>
-    </div>`;
+  socket.on("createMessage", (message, userName) => {
+    messages.innerHTML += `
+      <div class="message">
+        <b><i class="far fa-user-circle"></i> <span>${userName === user ? "me" : userName}</span></b>
+        <span>${message}</span>
+      </div>`;
+  });
 });
